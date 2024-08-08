942bb65b786ad559d631ea42ed66fb61229b5141
Ritikarajwar <ritikarajwar0@gmail.com>
2024-08-08 15:58:13 +0530

    ..

--- connection.js
+++ connection.js
@@ -2,7 +2,8 @@ import{MongoClient} from "mongodb"
 import dotenv from "dotenv"
 
 dotenv.config()
-console.log(process.env.MONGODB_URI)
+console.log("MongoDB URI:", process.env.MONGODB_URI);
+// console.log(process.env.connection)
 const connection = MongoClient.connect(process.env.MONGODB_URI);
 export const dbName = "myapi"
 export default connection
