0956016efb105cf320e72ada4f8d039823bd695d
Ritikarajwar <ritikarajwar0@gmail.com>
2024-08-16 23:08:23 +0530

    additional changes

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ dotenv.config()
 // console.log("MongoDB URI:", process.env.MONGODB_URI);
 // console.log(process.env.connection)
 const connection = MongoClient.connect(process.env.MONGODB_URI)
-// const connection = MongoClient.connect("mongodb+srv://ritikarajwar0:11223344@cluster0.u2mqpbq.mongodb.net/myapi?retryWrites=true&w=majority&appName=Cluster0")
+
 
 export const dbName = "myapi"
 export default connection
